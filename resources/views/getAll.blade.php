772c4bc0a307eb7b22937d0ccca4230fe58042f8
Jihanra <jihanrahadatulasy@gmail.com>
2021-09-22 15:17:44 +0700

    Minggu4

---
+++ resources/views/getAll.blade.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1>Ini adalah halaman GetAll <br></h1>
+    
+<p>{{$Article}} <p>
+</body>
+</html>
---
+++ resources/views/getById.blade.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<h1 align = center>Ini adalah halaman data Artikel</h1><br>
+
+    <table border = "3">
+        <tr align = center>
+            <td>ID</td>
+            <td>Title</td>
+            <td>Content</td>
+            <td>Image</td>
+            <td>Create At</td>
+            <td>Update At</td>
+        </tr>
+        <tr>
+            <td>{{$Article->id}}</td>
+            <td>{{$Article->title}}</td>
+            <td>{{$Article->content}}</td>
+            <td>{{$Article->image}}</td>
+            <td>{{$Article->created_at}}</td>
+            <td>{{$Article->updated_at}}</td>
+        </tr>
+    </table>
+</body>
+</html>
